2091f2c35420c926796e06cc541c2ab007f0ed90
Ming Li <headhuntar@gmail.com>
2018-09-24 19:54:50 -0700

    added some notes about math calculation

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserProfileComponent } from '../user-profile/user-profile.component';
 import { DashboardComponent } from '../dashboard/dashboard.component';
-import {ShiftsComponent} from '../shifts/shifts.component';
-import {CoverageComponent} from '../coverage/coverage.component';
-import {ShiftpickComponent} from '../shiftpick/shiftpick.component';
+import { ShiftsComponent } from '../shifts/shifts.component';
+import { CoverageComponent } from '../coverage/coverage.component';
+import { ShiftpickComponent } from '../shiftpick/shiftpick.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '', redirectTo: '/calculator', pathMatch: 'full' },
   { path: 'profile', component: UserProfileComponent },
-  { path: 'dashboard', component: DashboardComponent },
+  { path: 'calculator', component: DashboardComponent },
   { path: 'shifts', component: ShiftsComponent },
   { path: 'coverage', component: CoverageComponent },
   { path: 'shiftpick', component: ShiftpickComponent }
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
   dateObj: Date = new Date();
+  slotSelected: String = 'Select a slot';
 
   constructor() { }
 
   ngOnInit() {
+    // // odds are
+    // slot 1 - 1/9 
+    // slot 2 - 1/10
+    // slot 3 - 1/9
+    // slot 4 - 1/10 
+    // slot 5 - 1/10
+    // slot 6 - 1/10
+
+    // inate stat minutes 1
+
+    // quad roll 1/30/30/30/30/30
+
+    // spd roll max 1 / 3
   }
 }
