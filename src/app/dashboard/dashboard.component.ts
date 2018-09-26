e5ad6fe4d4cd0a4b89edab7acdf7e63c1df2dd33
Ming Li <headhuntar@gmail.com>
2018-09-25 17:47:51 -0700

    edit on dashboard page, able to pick rune, pick sub stat and pick inate stat still some linking to angular needed

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -8,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class DashboardComponent implements OnInit {
   dateObj: Date = new Date();
   slotSelected: String = 'Select a slot';
-
+  inateSelected: String = 'None';
+  statTypes: Array<String> = ['ATT %', 'ATT +', 'HP %', 'HP +', 'DEF %', 'DEF +', 'SPD', 'CRIT', 'CDMG', 'RES', 'ACC'];
+  slotNumbers: Array<String> = ['Slot 1', 'Slot 2', 'Slot 3', 'Slot 4', 'Slot 5', 'Slot 6'];
   constructor() { }
 
   ngOnInit() {
@@ -26,4 +28,29 @@ export class DashboardComponent implements OnInit {
 
     // spd roll max 1 / 3
   }
+
+  setInateSelected = (stat: String) => {
+    this.inateSelected = stat;
+  }
+
+  setSlotSelected = (slot: String) => {
+    this.slotSelected = slot;
+    // set primary stats possible based on slot choice
+    switch (slot) {
+      case 'Slot 1':
+        console.log('slot 1 selected');
+        return;
+      case 'Slot 2':
+        return;
+      case 'Slot 3':
+        return;
+      case 'Slot 4':
+        return;
+      case 'Slot 5':
+        return;
+      case 'Slot 6':
+        return;
+    }
+  }
+
 }
